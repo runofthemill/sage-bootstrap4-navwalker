13f49d2a66ea7c50d6295ac57cb0bf9e26d6bc8c
jmills <runofthemill@users.noreply.github.com>
2018-05-03 22:25:32 -0700

    allow dropdown parent to have link

--- bootstrap4-navwalker.php
+++ bootstrap4-navwalker.php
@@ -113,8 +113,8 @@ class wp_bootstrap4_navwalker extends \Walker_Nav_Menu {
 				$atts['class']			= 'nav-link';
 				if ( $args->has_children ){
 					$classes[] = ' dropdown';
-					$atts['href']   		= '#';
-					$atts['data-toggle']	= 'dropdown';
+// 					$atts['href']   		= '#';
+// 					$atts['data-toggle']	= 'dropdown';
 					$atts['class']			= 'dropdown-toggle nav-link';
 					$atts['role']	= 'button';
 					$atts['aria-haspopup']	= 'true';
